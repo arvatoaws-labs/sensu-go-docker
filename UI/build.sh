e113496fd170dc03faead1e636b69ac11021ff35
Patrick Robinson <patrick.robinson@bertelsmann.de>
2021-02-03 12:53:41 +0100

    FIX
    Signed-off-by: Patrick Robinson <patrick.robinson@bertelsmann.de>

--- UI/build.sh
+++ UI/build.sh
@@ -2,4 +2,4 @@
 
 VERSION=1.0.5
 
-docker buildx build -t ghcr.io/arvatoaws-labs/sensu-go-ui:$VERSION --platform linux/amd64,linux/arm64 --build-arg UI_VERSION=$VERSION --push .
+docker buildx build -t ghcr.io/arvatoaws-labs/sensu-go-ui:$VERSION --platform linux/amd64 --build-arg UI_VERSION=$VERSION --push .
