68b7122cd76a0b2039d382c5d6a7b82ba67cf460
Patrick Robinson <patrick.robinson@bertelsmann.de>
2021-02-03 12:55:45 +0100

    Bump version
    Signed-off-by: Patrick Robinson <patrick.robinson@bertelsmann.de>

--- sensu/build.sh
+++ sensu/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-VERSION=6.2.4
+VERSION=6.2.5
 
 docker buildx build -t ghcr.io/arvatoaws-labs/sensu-go:$VERSION --platform linux/amd64,linux/arm64 --build-arg SENSU_VERSION=$VERSION --push .
