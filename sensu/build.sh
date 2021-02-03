da6c37e6760beadbed87237532a7c7fa9e29813c
Philipp Hellmich <philipp.hellmich@bertelsmann.de>
2021-02-03 15:32:14 +0100

    added sns transport

--- sensu/build.sh
+++ sensu/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 VERSION=6.2.5
+SNS_VERSION=0.3.0
 
-docker buildx build -t ghcr.io/arvatoaws-labs/sensu-go:$VERSION --platform linux/amd64,linux/arm64 --build-arg SENSU_VERSION=$VERSION --push .
+docker buildx build -t ghcr.io/arvatoaws-labs/sensu-go:$VERSION --platform linux/amd64,linux/arm64 --build-arg SENSU_VERSION=$VERSION --build-arg SENSU_SNS_VERSION=$SNS_VERSION --push .
